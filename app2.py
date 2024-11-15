4291a4c7d1234d408b6a9bb4043d4140b2e1b695
Arnaubadia-tech <110681428+Arnaubadia-tech@users.noreply.github.com>
2024-11-15 11:14:50 +0100

    Update app2.py

--- app2.py
+++ app2.py
@@ -223,7 +223,7 @@ merged_poverty = pd.merge(poverty, state_aggregates, on='State')
 
 # Graph
 scatter_plot2 = alt.Chart(merged_poverty).mark_point(filled=True).encode(
-    x=alt.X('PovertyRatesPercentOfPopulationBelowPovertyLevel:Q', title='Poverty Rate (%)',scale=alt.Scale(domain=[min(merged_poverty['PovertyRatesPercentOfPopulationBelowPovertyLevel']) - 1, None])),
+    x=alt.X('PovertyRatesPercentOfPopulationBelowPovertyLevel:Q', title='Poverty Rate (%)'),
     y=alt.Y('per_100k:Q', title='School Shootings per 100k'),
     tooltip=['State:N', 'PovertyRatesPercentOfPopulationBelowPovertyLevel:Q', 'per_100k:Q'],
     size='population:Q'  # Tamaño opcional basado en la población
@@ -251,7 +251,7 @@ merged_mental = pd.merge(mental, state_aggregates, on='State')
 
 # Crear el gráfico de dispersión
 scatter_plot3 = alt.Chart(merged_mental).mark_point(filled=True).encode(
-    x=alt.X('MentalHealthStatisticsRatesOfMentalIllness:Q', title='Mental Illness (%)',scale=alt.Scale(domain=[min(merged_mental['MentalHealthStatisticsRatesOfMentalIllness']) - 1, None])),
+    x=alt.X('MentalHealthStatisticsRatesOfMentalIllness:Q', title='Mental Illness (%)'),
     y=alt.Y('per_100k:Q', title='Mass Shootings per 100k'),
     tooltip=['State:N', 'MentalHealthStatisticsRatesOfMentalIllness:Q', 'per_100k:Q'],
     size='population:Q'  # Tamaño opcional basado en la población
