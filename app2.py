2f2f56c838bbf1015dac7bae477cdfd5327ed5cd
Arnaubadia-tech <110681428+Arnaubadia-tech@users.noreply.github.com>
2024-11-17 18:03:38 +0100

    Update app2.py

--- app2.py
+++ app2.py
@@ -18,11 +18,11 @@ mental = pd.read_csv('mental_ill.csv')
 finalcounties = pd.read_csv('finalcounties.csv')
 
 finalstates = pd.read_csv('Q2.1dataset.csv')
-barchart = pd.read_csv('Q1dataset.csv')
+barchart = pd.read_csv('Q1dataset (3).csv')
 scatterplot = pd.read_csv('Q3.2dataset.csv')
 comparison = pd.read_csv('Q4dataset.csv')
-povertydata = pd.read_csv('Q5.1dataset.csv')
-mentaldata = pd.read_csv('Q5.2dataset.csv')
+povertydata = pd.read_csv('Q5.1dataset (1).csv')
+mentaldata = pd.read_csv('Q5.2dataset (2).csv')
 
 ## Mass Shootings per Capita by State (per 100k)
 state_bar_chart2 = alt.Chart(barchart).mark_bar().encode(
